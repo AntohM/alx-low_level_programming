ab008e6283aed3e733b1cfed4d634689ccb4ab76
AntohM <antohmarcus4@gmail.com>
2022-04-08 19:42:46 +0300

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
- *my hello.prinf task
- *Return :return 1 after program execution
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
